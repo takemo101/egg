fccd591921c0ccb8ed93f6b2b38b03cdb0fc8d94
takemo101 <takemo101@gmail.com>
2023-02-04 11:21:44 +0900

    設定修正

--- setting/function.php
+++ setting/function.php
@@ -2,9 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Response;
 use Takemo101\Egg\Routing\RouteBuilder;
-use Takemo101\Egg\Routing\RouterContract;
 use Takemo101\Egg\Support\Hook\Hook;
-use Takemo101\Egg\Support\Log\Loggers;
 use Takemo101\Egg\Support\StaticContainer;
 
 /** @var Hook */
