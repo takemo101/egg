744947e90abb99cdda154cd45ea37d9a2399b723
takemo101 <takemo101@gmail.com>
2023-02-04 12:05:07 +0900

    READMEに色々追記

--- setting/filter.php
+++ setting/filter.php
@@ -3,6 +3,9 @@
 use Takemo101\Egg\Http\Filter\MethodOverrideFilter;
 use Takemo101\Egg\Http\Filter\SessionFilter;
 
+/**
+ * @return array<object|mixed[]|class-string>
+ */
 return [
     MethodOverrideFilter::class,
     SessionFilter::class,
