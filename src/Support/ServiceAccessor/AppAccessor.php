d76e43a94055a26a46e570ddf1cfc21e3402b426
takemo101 <takemo101@gmail.com>
2023-05-09 22:21:54 +0900

    AppAccessorに足りないアノテーションを追加

--- src/Support/ServiceAccessor/AppAccessor.php
+++ src/Support/ServiceAccessor/AppAccessor.php
@@ -8,6 +8,14 @@ namespace Takemo101\Egg\Support\ServiceAccessor;
  * @method static \Takemo101\Egg\Kernel\ApplicationEnvironment env()
  * @method static \Takemo101\Egg\Kernel\ApplicationPath path()
  * @method static \Takemo101\Egg\Support\Injector\ContainerContract container()
+ * @method static \Takemo101\Egg\Support\Injector\ContainerContract alias(string $class, string $alias)
+ * @method static \Takemo101\Egg\Support\Injector\ContainerContract instance(string $label, mixed $instance)
+ * @method static \Takemo101\Egg\Support\Injector\ContainerContract singleton(string $label, Closure|string|null $callback = null)
+ * @method static \Takemo101\Egg\Support\Injector\ContainerContract bind(string $label, Closure|string|null $callback = null)
+ * @method static boolean has(string $label)
+ * @method static void clear()
+ * @method static mixed make(string $label, mixed[] $options = [])
+ * @method static mixed call(callable $callable, mixed[] $options = [])
  * @see \Takemo101\Egg\Kernel\Application
  */
 final class AppAccessor extends ServiceAccessor
